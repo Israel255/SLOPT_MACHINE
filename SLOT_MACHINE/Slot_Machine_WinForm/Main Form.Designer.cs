af53b636e04dcf5ad9566bee0cad830bca6883af
Israel255 <62959042+Israel255@users.noreply.github.com>
2020-06-09 11:12:09 +0300

    Add files via upload

---
+++ SLOT_MACHINE/Slot_Machine_WinForm/Main Form.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Slot_Machine_WinForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.symbol1_pcbx = new System.Windows.Forms.PictureBox();
+            this.symbol2_pcbx = new System.Windows.Forms.PictureBox();
+            this.symbol3_pcbx = new System.Windows.Forms.PictureBox();
+            this.bet_lbl = new System.Windows.Forms.Label();
+            this.money_lbl = new System.Windows.Forms.Label();
+            this.bet_txtbx = new System.Windows.Forms.TextBox();
+            this.current_money_lbl = new System.Windows.Forms.Label();
+            this.jupingPicTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.symbol1_pcbx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.symbol2_pcbx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.symbol3_pcbx)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // symbol1_pcbx
+            // 
+            this.symbol1_pcbx.Location = new System.Drawing.Point(12, 12);
+            this.symbol1_pcbx.Name = "symbol1_pcbx";
+            this.symbol1_pcbx.Size = new System.Drawing.Size(217, 186);
+            this.symbol1_pcbx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.symbol1_pcbx.TabIndex = 0;
+            this.symbol1_pcbx.TabStop = false;
+            // 
+            // symbol2_pcbx
+            // 
+            this.symbol2_pcbx.Location = new System.Drawing.Point(303, 12);
+            this.symbol2_pcbx.Name = "symbol2_pcbx";
+            this.symbol2_pcbx.Size = new System.Drawing.Size(217, 186);
+            this.symbol2_pcbx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.symbol2_pcbx.TabIndex = 1;
+            this.symbol2_pcbx.TabStop = false;
+            // 
+            // symbol3_pcbx
+            // 
+            this.symbol3_pcbx.Location = new System.Drawing.Point(571, 12);
+            this.symbol3_pcbx.Name = "symbol3_pcbx";
+            this.symbol3_pcbx.Size = new System.Drawing.Size(217, 186);
+            this.symbol3_pcbx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.symbol3_pcbx.TabIndex = 2;
+            this.symbol3_pcbx.TabStop = false;
+            // 
+            // bet_lbl
+            // 
+            this.bet_lbl.AutoSize = true;
+            this.bet_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.bet_lbl.Location = new System.Drawing.Point(32, 223);
+            this.bet_lbl.Name = "bet_lbl";
+            this.bet_lbl.Size = new System.Drawing.Size(197, 46);
+            this.bet_lbl.TabIndex = 3;
+            this.bet_lbl.Text = "Youre Bet";
+            // 
+            // money_lbl
+            // 
+            this.money_lbl.AutoSize = true;
+            this.money_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.money_lbl.Location = new System.Drawing.Point(531, 223);
+            this.money_lbl.Name = "money_lbl";
+            this.money_lbl.Size = new System.Drawing.Size(257, 46);
+            this.money_lbl.TabIndex = 4;
+            this.money_lbl.Text = "Youre Money";
+            // 
+            // bet_txtbx
+            // 
+            this.bet_txtbx.Location = new System.Drawing.Point(72, 291);
+            this.bet_txtbx.Name = "bet_txtbx";
+            this.bet_txtbx.Size = new System.Drawing.Size(113, 20);
+            this.bet_txtbx.TabIndex = 5;
+            this.bet_txtbx.Text = "0";
+            this.bet_txtbx.KeyDown += new System.Windows.Forms.KeyEventHandler(this.bet_txtbx_KeyDown);
+            // 
+            // current_money_lbl
+            // 
+            this.current_money_lbl.AutoSize = true;
+            this.current_money_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.current_money_lbl.Location = new System.Drawing.Point(620, 282);
+            this.current_money_lbl.Name = "current_money_lbl";
+            this.current_money_lbl.Size = new System.Drawing.Size(86, 46);
+            this.current_money_lbl.TabIndex = 6;
+            this.current_money_lbl.Text = "200";
+            // 
+            // jupingPicTimer
+            // 
+            this.jupingPicTimer.Enabled = true;
+            this.jupingPicTimer.Interval = 10000;
+            this.jupingPicTimer.Tick += new System.EventHandler(this.jupingPicTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.current_money_lbl);
+            this.Controls.Add(this.bet_txtbx);
+            this.Controls.Add(this.money_lbl);
+            this.Controls.Add(this.bet_lbl);
+            this.Controls.Add(this.symbol3_pcbx);
+            this.Controls.Add(this.symbol2_pcbx);
+            this.Controls.Add(this.symbol1_pcbx);
+            this.Name = "MainForm";
+            this.Text = "Slot Machine";
+            ((System.ComponentModel.ISupportInitialize)(this.symbol1_pcbx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.symbol2_pcbx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.symbol3_pcbx)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox symbol1_pcbx;
+        private System.Windows.Forms.PictureBox symbol2_pcbx;
+        private System.Windows.Forms.PictureBox symbol3_pcbx;
+        private System.Windows.Forms.Label bet_lbl;
+        private System.Windows.Forms.Label money_lbl;
+        private System.Windows.Forms.TextBox bet_txtbx;
+        private System.Windows.Forms.Label current_money_lbl;
+        private System.Windows.Forms.Timer jupingPicTimer;
+    }
+}
+
---
+++ SLOT_MACHINE/Slot_Machine_WinForm/Main Form.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using WMPLib;
+using System.Threading;
+
+namespace Slot_Machine_WinForm
+{
+    public partial class MainForm : Form
+    {
+        int new_money = 0;
+        int convert_txt = 0;
+        int convert_lbl = 0;
+        const int MAX_MONEY = 1000;
+        Random num = new Random();
+        const int MAX_RANDOM = 6;
+        const int MIN_RANDOM = 1;
+        int random = 0;
+        const int DOWN_UP_THE_BET = 10;
+        const int MIN_MONEY = 0;
+        const int NUM_PIC = 3;
+        const int RONALDO = 1;
+        const int KROOS = 2;
+        const int FIRMINO = 3;
+        const int NEYMAR = 4;
+        const int BENAYOUN = 5;
+        const int HAT_TRICK = 7;
+        const int PAIR = 2;
+        public MainForm()
+        {
+            InitializeComponent();
+            bet_txtbx.ReadOnly = true;
+        }
+
+        private void bet_txtbx_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Money_Minus_Bet();
+                Randon_PIC();
+                Compare_The_Tags();
+                End_Game();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                Up_The_Bet();
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                Down_The_Bet();
+            }
+        }
+        private void Up_The_Bet()
+        {
+            convert_txt = Convert.ToInt32(bet_txtbx.Text);
+            convert_txt += DOWN_UP_THE_BET;
+            bet_txtbx.Text = convert_txt.ToString();
+            if (Convert.ToInt32(bet_txtbx.Text) < MIN_MONEY || Convert.ToInt32(bet_txtbx.Text) > Convert.ToInt32(current_money_lbl.Text))
+            {
+                MessageBox.Show("Come on be serious!");
+                bet_txtbx.Text = "0";
+            }
+        }
+        private void Down_The_Bet()
+        {
+            convert_txt = Convert.ToInt32(bet_txtbx.Text);
+            convert_txt -= DOWN_UP_THE_BET;
+            bet_txtbx.Text = convert_txt.ToString();
+            if (Convert.ToInt32(bet_txtbx.Text) < MIN_MONEY || Convert.ToInt32(bet_txtbx.Text) > Convert.ToInt32(current_money_lbl.Text))
+            {
+                MessageBox.Show("Come on be serious!");
+                bet_txtbx.Text = "0";
+            }
+        }
+        private void End_Game()
+        {
+            if (Convert.ToInt32(current_money_lbl.Text) == MIN_MONEY)
+            {
+                WindowsMediaPlayer lost = new WindowsMediaPlayer();
+                lost.URL = "MosheCohen-MishakeyKlafim.mp3";
+                lost.controls.play();
+                MessageBox.Show("You lost you're money");
+                this.Close();
+
+            }
+            else if (Convert.ToInt32(current_money_lbl.Text) >= MAX_MONEY)
+            {
+                WindowsMediaPlayer we_are_the_champions = new WindowsMediaPlayer();
+                we_are_the_champions.URL = "Queen_We_Are_The_Champions_Ringtone_(by Fringster.com).mp3";
+                we_are_the_champions.controls.play();
+                MessageBox.Show("We ran out of money!");
+                this.Close();
+            }
+        }
+        private void Randon_PIC()
+        {
+            for (int i = 0; i < NUM_PIC; i++)
+            {
+                    random = num.Next(MIN_RANDOM, MAX_RANDOM);
+                    if (i == 0)
+                    {
+                        if (random == 1)
+                        {
+                            symbol1_pcbx.Image = Image.FromFile("7.jpeg");
+                            symbol1_pcbx.Tag = RONALDO;
+                        }
+                        else if (random == 2)
+                        {
+                            symbol1_pcbx.Image = Image.FromFile("8.jpeg");
+                            symbol1_pcbx.Tag = KROOS;
+                        }
+                        else if (random == 3)
+                        {
+                            symbol1_pcbx.Image = Image.FromFile("9.jpeg");
+                            symbol1_pcbx.Tag = FIRMINO;
+                        }
+                        else if (random == 4)
+                        {
+                            symbol1_pcbx.Image = Image.FromFile("10.jpeg");
+                            symbol1_pcbx.Tag = NEYMAR;
+                        }
+                        else if (random == 5)
+                        {
+                            symbol1_pcbx.Image = Image.FromFile("15.jpg");
+                            symbol1_pcbx.Tag = BENAYOUN;
+                        }
+                    }
+                    if (i == 1)
+                    {
+                        if (random == 1)
+                        {
+                            symbol2_pcbx.Image = Image.FromFile("7.jpeg");
+                            symbol2_pcbx.Tag = RONALDO;
+                        }
+                        else if (random == 2)
+                        {
+                            symbol2_pcbx.Image = Image.FromFile("8.jpeg");
+                            symbol2_pcbx.Tag = KROOS;
+                        }
+                        else if (random == 3)
+                        {
+                            symbol2_pcbx.Image = Image.FromFile("9.jpeg");
+                            symbol2_pcbx.Tag = FIRMINO;
+                        }
+                        else if (random == 4)
+                        {
+                            symbol2_pcbx.Image = Image.FromFile("10.jpeg");
+                            symbol2_pcbx.Tag = NEYMAR;
+                        }
+                        else if (random == 5)
+                        {
+                            symbol2_pcbx.Image = Image.FromFile("15.jpg");
+                            symbol2_pcbx.Tag = BENAYOUN;
+                        }
+                    }
+                    if (i == 2)
+                    {
+                        if (random == 1)
+                        {
+                            symbol3_pcbx.Image = Image.FromFile("7.jpeg");
+                            symbol3_pcbx.Tag = RONALDO;
+                        }
+                        else if (random == 2)
+                        {
+                            symbol3_pcbx.Image = Image.FromFile("8.jpeg");
+                            symbol3_pcbx.Tag = KROOS;
+                        }
+                        else if (random == 3)
+                        {
+                            symbol3_pcbx.Image = Image.FromFile("9.jpeg");
+                            symbol3_pcbx.Tag = FIRMINO;
+                        }
+                        else if (random == 4)
+                        {
+                            symbol3_pcbx.Image = Image.FromFile("10.jpeg");
+                            symbol3_pcbx.Tag = NEYMAR;
+                        }
+                        else if (random == 5)
+                        {
+                            symbol3_pcbx.Image = Image.FromFile("15.jpg");
+                            symbol3_pcbx.Tag = BENAYOUN;
+                        }
+                    }
+            }
+        }
+        private void Compare_The_Tags()
+        {
+            if (Convert.ToInt32(symbol1_pcbx.Tag) == Convert.ToInt32(symbol2_pcbx.Tag) && Convert.ToInt32(symbol1_pcbx.Tag) == Convert.ToInt32(symbol3_pcbx.Tag))
+            {
+                convert_txt = Convert.ToInt32(bet_txtbx.Text);
+                convert_txt *= HAT_TRICK;
+                new_money += convert_txt;
+                current_money_lbl.Text = new_money.ToString();
+            }
+            if (Convert.ToInt32(symbol1_pcbx.Tag) == Convert.ToInt32(symbol2_pcbx.Tag) || Convert.ToInt32(symbol1_pcbx.Tag) == Convert.ToInt32(symbol3_pcbx.Tag) || Convert.ToInt32(symbol2_pcbx.Tag) == Convert.ToInt32(symbol3_pcbx.Tag))
+            {
+                convert_txt = Convert.ToInt32(bet_txtbx.Text);
+                convert_txt *= PAIR;
+                new_money += convert_txt;
+                current_money_lbl.Text = new_money.ToString();
+            }
+            if (Convert.ToInt32(symbol1_pcbx.Tag) == BENAYOUN || Convert.ToInt32(symbol2_pcbx.Tag) == BENAYOUN || Convert.ToInt32(symbol3_pcbx.Tag) == BENAYOUN)
+            {
+                convert_txt = Convert.ToInt32(bet_txtbx.Text);
+                new_money += convert_txt;
+                current_money_lbl.Text = new_money.ToString();
+            }
+        }
+        private void Money_Minus_Bet()
+        {
+            convert_txt = Convert.ToInt32(bet_txtbx.Text);
+            convert_lbl = Convert.ToInt32(current_money_lbl.Text);
+            new_money = convert_lbl - convert_txt;
+            current_money_lbl.Text = new_money.ToString();
+        }
+
+        private void jupingPicTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
